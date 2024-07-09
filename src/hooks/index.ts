4dfc6cbd242d5ff18b61a097e934ddf7fc585012
Mariano Malomo <mariano.malomo@gmail.com>
2024-07-09 11:22:49 -0300

    export hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,5 @@
 export { useCountdown } from "./use-countdown";
 export { useIdle } from "./use-idle";
+export { useLoadingDots } from "./use-loading-dots";
 export { useTranslations } from "./useTranslations";
 
