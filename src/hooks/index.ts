1328640e8127bae0bfa75735dea58fee63f1cd35
Mariano Malomo <mariano.malomo@gmail.com>
2024-07-15 10:28:10 -0300

    rename file

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,5 @@
 export { useCountdown } from "./use-countdown";
 export { useIdle } from "./use-idle";
 export { useLoadingDots } from "./use-loading-dots";
-export { useTranslations } from "./useTranslations";
+export { useTranslations } from "./use-translations";
 
