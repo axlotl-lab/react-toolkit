4ae6457645dfa5bff4410fdf363c47b34a2a8e4d
Mariano Malomo <mariano.malomo@gmail.com>
2024-07-29 10:30:32 -0300

    install jest

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+import type { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: false,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  moduleFileExtensions: [
+    "js",
+    "mjs",
+    "cjs",
+    "jsx",
+    "ts",
+    "tsx",
+    "json",
+    "node"
+  ],
+  preset: "ts-jest"
+};
+
+export default config;
