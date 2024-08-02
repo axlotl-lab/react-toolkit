fbca7455ed1bb03522ebdd0f1f69581265529ac8
Mariano Malomo <mariano.malomo@gmail.com>
2024-08-02 10:15:44 -0300

    reduce types deeph

--- src/hooks/translations/types.ts
+++ src/hooks/translations/types.ts
@@ -3,7 +3,7 @@ type Join<K, P> = K extends string | number ?
   `${K}${"" extends P ? "" : "."}${P}`
   : never : never;
 
-type Prev = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ...0[]];
+type Prev = [never, 0, 1, 2, 3, 4, 5, 6, ...0[]];
 
 export type NestedTranslations<T> = {
   [K in keyof T]: T[K] extends Record<string, string>
@@ -11,7 +11,7 @@ export type NestedTranslations<T> = {
   : NestedTranslations<T[K]>
 };
 
-export type FlattenObjectKeys<T, D extends number = 10> = [D] extends [never] ? never : T extends object ?
+export type FlattenObjectKeys<T, D extends number = 6> = [D] extends [never] ? never : T extends object ?
   { [K in keyof T]: K extends string | number ?
     T[K] extends Record<string, string> ? `${K}` :
     T[K] extends object ? Join<K, FlattenObjectKeys<T[K], Prev[D]>> :
