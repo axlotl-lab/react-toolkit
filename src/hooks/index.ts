db9317bbe3784b4e0ec1363acb67f35dd7f581c6
Mariano Malomo <mariano.malomo@gmail.com>
2024-10-23 09:19:22 -0300

    add missing export in index

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useCountdown } from "./use-countdown";
+export { useDebounce } from "./use-debounce";
 export { useIdle } from "./use-idle";
 export { useLoadingDots } from "./use-loading-dots";
